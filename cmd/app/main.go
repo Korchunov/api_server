67e7f291eb3fca3f3a8f413424d9cf50f8192f7e
KNS <n.s.korchunov@gmail.com>
2023-11-30 23:09:30 +0300

    config

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,39 @@
-package app
+package main
+
+import (
+	"fmt"
+	"github.com/Korchunov/api_server.git/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+	log.Info("активирована инфа", slog.String("env", cfg.Env))
+	log.Debug("активирован дебаг")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(
+			slog.NewTextHandler(
+				os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		slog.NewJSONHandler(
+			os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo})
+	}
+	return log
+}
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,39 @@
 package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+	"time"
+)
+
+type conf struct {
+	Env         string `yaml:"env" env-default:"local" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_Timeout" env-default:"60s"`
+}
+
+// Must... означает, что процедура будет возвращать не ошибку, а Fatal.
+// Это как раз актуально при первом запуске приложения
+func MustLoad() *conf {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+	// Stat возвращает структуру FileInfo, описывающую файл.
+	//Если есть ошибка, она будет иметь тип *PathError.
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatal("config fatal %s does not exist", configPath)
+	}
+	var cfg conf
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatal("cannot read config: %s", err)
+	}
+	return &cfg
+}
